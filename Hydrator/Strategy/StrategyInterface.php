9e514056057f9dcf3e7bf192cf97d28a9ee2914c
Matthew Weier O'Phinney <matthew@zend.com>
2013-06-12 14:46:41 -0500

    zendframework/zf2@e56eefe5f27dd6dbe9c407bf56efea0763783db4 (1369417928)

--- Hydrator/Strategy/StrategyInterface.php
+++ Hydrator/Strategy/StrategyInterface.php
@@ -9,16 +9,12 @@
 
 namespace Zend\Stdlib\Hydrator\Strategy;
 
-/**
- * @todo v3.0, add optional object/data to extract/hydrate.
- */
 interface StrategyInterface
 {
     /**
      * Converts the given value so that it can be extracted by the hydrator.
      *
-     * @param mixed   $value The original value.
-     * @param object $object (optional) The original object for context.
+     * @param mixed $value The original value.
      * @return mixed Returns the value that should be extracted.
      */
     public function extract($value);
@@ -27,7 +23,6 @@ interface StrategyInterface
      * Converts the given value so that it can be hydrated by the hydrator.
      *
      * @param mixed $value The original value.
-     * @param array  $data (optional) The original data for context.
      * @return mixed Returns the value that should be hydrated.
      */
     public function hydrate($value);
