8094381dc9715042991cb94fed3c6fe14e6b15ab
Adam Lundrigan <adam@lundrigan.ca>
2015-09-23 10:16:44 -0230

    Test case to enforce acceptance of deprecated HydratorInterface into methods typehinted on deprecated HydrationInterface and ExtractorInterface

--- test/HydratorDeprecationTest.php
+++ test/HydratorDeprecationTest.php
@@ -34,4 +34,26 @@ class HydratorDeprecationTest extends TestCase
         $hydratorInjected->setHydrator($hydrator);
         $this->assertSame($hydrator, $hydratorInjected->hydrator);
     }
+
+    public function testDeprecatedHydratorInterfaceIsAcceptedByMethodsTypehintedWithDeprecatedHydrationInterface()
+    {
+        $hydratorInjected = new TestAsset\HydratorInjectedObjectUsingDeprecatedHydrationInterfaceTypehint();
+        $hydrator         = new TestAsset\DeprecatedInterfaceHydrator();
+        set_error_handler(function ($errno, $errstr) {
+            $this->fail('Catchable fatal error was triggered: ' . $errstr);
+        }, E_RECOVERABLE_ERROR);
+        $hydratorInjected->setHydrator($hydrator);
+        $this->assertSame($hydrator, $hydratorInjected->hydrator);
+    }
+
+    public function testDeprecatedHydratorInterfaceIsAcceptedByMethodsTypehintedWithDeprecatedExtractionInterface()
+    {
+        $hydratorInjected = new TestAsset\HydratorInjectedObjectUsingDeprecatedExtractionInterfaceTypehint();
+        $hydrator         = new TestAsset\DeprecatedInterfaceHydrator();
+        set_error_handler(function ($errno, $errstr) {
+            $this->fail('Catchable fatal error was triggered: ' . $errstr);
+        }, E_RECOVERABLE_ERROR);
+        $hydratorInjected->setExtractor($hydrator);
+        $this->assertSame($hydrator, $hydratorInjected->extractor);
+    }
 }
