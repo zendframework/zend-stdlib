08ab55c67ca6ba3b1044d53a78dd87ef3f9adbbf
KoKsPfLaNzE <kokspflanze@gmx.de>
2017-03-28 21:49:34 +0200

    removed old PHPUnit_Runner_Version case

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -10,17 +10,6 @@
  */
 error_reporting(E_ALL | E_STRICT);
 
-if (class_exists('PHPUnit_Runner_Version', true)) {
-    $phpUnitVersion = PHPUnit_Runner_Version::id();
-    if ('@package_version@' !== $phpUnitVersion && version_compare($phpUnitVersion, '4.0.0', '<')) {
-        echo 'This version of PHPUnit (' . PHPUnit_Runner_Version::id() . ') is not supported'
-           . ' in the zend-stdlib unit tests. Supported is version 4.0.0 or higher.'
-           . ' See also the CONTRIBUTING.md file in the component root.' . PHP_EOL;
-        exit(1);
-    }
-    unset($phpUnitVersion);
-}
-
 /**
  * Setup autoloading
  */
