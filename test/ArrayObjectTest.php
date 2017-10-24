5c2f528c5e3b775b960adc128efc7717ff2db64c
Remi Collet <remi@remirepo.net>
2017-10-24 10:49:02 +0200

    Fix testCount for PHP 7.2
    PHP 7.2 raise a deprecated message
      Parameter must be an array or an object that implements Countable
    
    I think this should not be hidden (count could take care of this),
    so this change declare the message as expected.
    
    A new test is added for object which are really countable

--- test/ArrayObjectTest.php
+++ test/ArrayObjectTest.php
@@ -103,8 +103,20 @@ class ArrayObjectTest extends TestCase
 
     public function testCount()
     {
+        if (version_compare(PHP_VERSION, '7.2', '>=')) {
+            $this->setExpectedException(
+                'PHPUnit_Framework_Error_Warning',
+                'Parameter must be an array or an object that implements Countable'
+            );
+        }
         $ar = new ArrayObject(new TestAsset\ArrayObjectObjectVars());
         $this->assertEquals(1, $ar->count());
+    }
+
+    public function testCountable()
+    {
+        $ar = new ArrayObject(new TestAsset\ArrayObjectObjectCount());
+        $this->assertEquals(42, $ar->count());
     }
 
     public function testExchangeArray()
