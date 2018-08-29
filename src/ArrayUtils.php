15e59b3e7e11fecedacc7daf51e7f40c4be9c2ad
Abdul Malik Ikhsan <samsonasik@gmail.com>
2018-08-29 09:05:23 +0700

    change to [] !== var comparison

--- src/ArrayUtils.php
+++ src/ArrayUtils.php
@@ -47,7 +47,7 @@ abstract class ArrayUtils
             return $allowEmpty;
         }
 
-        return ! empty(array_filter(array_keys($value), 'is_string'));
+        return [] !== array_filter(array_keys($value), 'is_string');
     }
 
     /**
@@ -67,7 +67,7 @@ abstract class ArrayUtils
             return $allowEmpty;
         }
 
-        return ! empty(array_filter(array_keys($value), 'is_int'));
+        return [] !== array_filter(array_keys($value), 'is_int');
     }
 
     /**
@@ -94,7 +94,7 @@ abstract class ArrayUtils
             return $allowEmpty;
         }
 
-        return ! empty(array_filter(array_keys($value), 'is_numeric'));
+        return [] !== array_filter(array_keys($value), 'is_numeric');
     }
 
     /**
